4661b594ee0b810290e862764a550504e79c6e6a
황종훈 <ybell1028@gmail.com>
2021-04-10 16:32:42 +0900

    Add : ERD 기반 Entity 클래스, JPA 레포지토리 생성

--- src/main/java/com/yapp18/retrospect/domain/user/User.java
+++ src/main/java/com/yapp18/retrospect/domain/user/User.java
@@ -1,4 +1,35 @@
 package com.yapp18.retrospect.domain.user;
 
-public class User {
+import com.yapp18.retrospect.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name="user_tb")
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_idx;
+    @Column(length = 20, nullable = false)
+    private String name;
+    @Column(length = 50, nullable = false)
+    private String email;
+    private String profile_url;
+    @Column(length = 20, nullable = false)
+    private String platform;
+    private String access_token;
+
+    @Builder
+    public User(String name, String email, String profile_url, String platform, String access_token) {
+        this.name = name;
+        this.email = email;
+        this.profile_url = profile_url;
+        this.platform = platform;
+        this.access_token = access_token;
+    }
 }
