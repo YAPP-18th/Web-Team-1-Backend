0bc2a8b52a66e44f3e7081d707b130c71029692b
황종훈 <ybell1028@gmail.com>
2021-04-17 10:38:56 +0900

    Add : Template Entity 클래스, JPA 레포지토리 생성

---
+++ src/main/java/com/yapp18/retrospect/domain/template/Template.java
@@ -0,0 +1,38 @@
+package com.yapp18.retrospect.domain.template;
+
+import com.yapp18.retrospect.domain.BaseTimeEntity;
+import com.yapp18.retrospect.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name="template_tb")
+public class Template extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long template_idx;
+
+    @Column(length = 30, nullable = false)
+    private String template_name;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "own_user_idx")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @Builder
+    public Template(String template_name, String content, User user) {
+        this.template_name = template_name;
+        this.content = content;
+        this.user = user;
+    }
+}
