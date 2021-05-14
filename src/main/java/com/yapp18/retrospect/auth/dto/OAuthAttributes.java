2c216e16821676972790b2bde5441568d28be0fc
황종훈 <ybell1028@gmail.com>
2021-05-14 15:13:24 +0900

    Fix : Oauth2 로그인 authorization_request_not_found 문제 해결 (not null 컬럼에 null 삽입이 원인, OAuthAttributes 수정)

--- src/main/java/com/yapp18/retrospect/auth/dto/OAuthAttributes.java
+++ src/main/java/com/yapp18/retrospect/auth/dto/OAuthAttributes.java
@@ -14,42 +14,46 @@ public class OAuthAttributes {
     private String name;
     private String email;
     private String picture;
+    private String platform;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture, String platform) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
         this.email = email;
         this.picture = picture;
+        this.platform = platform;
     }
 
     //OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야만 함
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
         if("naver".equals(registrationId)){ //네이버인지 판단하는 코드
-            return ofNaver("id", attributes);
+            return ofKakao(registrationId,"id", attributes);
         }
-        return ofGoogle(userNameAttributeName, attributes);
+        return ofGoogle(registrationId, userNameAttributeName, attributes);
     }
 
-    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+    private static OAuthAttributes ofGoogle(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
         //구글 생성자
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
+                .platform(registrationId)
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
 
-    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+    private static OAuthAttributes ofKakao(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
         return OAuthAttributes.builder()
                 .name((String) response.get("name"))
                 .email((String) response.get("email"))
-                .picture((String) response.get("profile_image"))
+                .picture((String) response.get("picture"))
+                .platform(registrationId)
                 .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
@@ -57,10 +61,12 @@ public class OAuthAttributes {
 
     public User toEntity() {
         return User.builder()
+                .role(Role.MEMBER)
                 .name(name)
+                .nickname(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.MEMBER)
+                .platform(platform)
                 .build();
     }
 }
