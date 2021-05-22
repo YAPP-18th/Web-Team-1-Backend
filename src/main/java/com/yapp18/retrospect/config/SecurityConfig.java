7f885b476a219cdaa4b230757afb023c227e18c2
황종훈 <ybell1028@gmail.com>
2021-05-22 12:53:08 +0900

    Add : 배포 스크립트 실행을 위한 /profile URL 허용

--- src/main/java/com/yapp18/retrospect/config/SecurityConfig.java
+++ src/main/java/com/yapp18/retrospect/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .headers().frameOptions().disable() //h2-console 화면을 사용하기 위해 해당 옵션들을 disable 합니다.
                 .and()
                     .authorizeRequests() // URL 별 권한 관리를 설정하는 옵션의 시작점입니다. authorizeRequests가 선언되어야만 antMatchers 옵션을 사용할 수 있습니다.
-                        .antMatchers("/", "/css/**", "/image/**", "/js/**", "/h2-console/**").permitAll()
+                        .antMatchers("/", "/css/**", "/image/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                         .antMatchers("/signin", "/signup").permitAll()
                         .antMatchers("/api/v1/posts/lists", "/api/v1/posts/idx", "/api/v1/posts/search",
                                 "/v2/api-docs", "/swagger-resources/**",
