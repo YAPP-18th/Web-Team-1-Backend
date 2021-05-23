90a8c538a4e7b88c25d96aefd5d7b46d4cd42e0f
HurJungMin <gjwjdlas@naver.com>
2021-05-23 21:07:22 +0900

    Add : OriginPattern으로 변경

--- src/main/java/com/yapp18/retrospect/config/SecurityConfig.java
+++ src/main/java/com/yapp18/retrospect/config/SecurityConfig.java
@@ -110,7 +110,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOriginPattern("http://localhost:3000");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
