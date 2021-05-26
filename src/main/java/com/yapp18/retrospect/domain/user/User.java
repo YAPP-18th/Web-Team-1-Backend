8d4829639180bffb2048c8104992585c106ef95b
황종훈 <ybell1028@gmail.com>
2021-05-26 20:12:01 +0900

    Add : User Profile 조회, 수정 API 구현

--- src/main/java/com/yapp18/retrospect/domain/user/User.java
+++ src/main/java/com/yapp18/retrospect/domain/user/User.java
@@ -1,6 +1,7 @@
 package com.yapp18.retrospect.domain.user;
 
 import com.yapp18.retrospect.domain.BaseTimeEntity;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,10 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@NoArgsConstructor
 @Getter
 @Table(name="user_tb")
+// 기본 생성자 접근을 protected으로 변경하면 외부에서 해당 생성자를 접근 할 수 없으므로 Builder를 통해서만 객체 생성 가능하므로 안전성 보장
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -56,9 +58,17 @@ public class User extends BaseTimeEntity {
         this.job = job;
     }
 
-    public User update(String name, String profile){
+    public User simpleUpdate(String name, String profile){
         this.name = name;
         this.profile = profile;
+        return this;
+    }
+
+    public User updateProfile(String profile, String nickname, String job, String intro){
+        this.profile = profile;
+        this.nickname = nickname;
+        this.job = job;
+        this.intro = intro;
         return this;
     }
 
--- src/main/java/com/yapp18/retrospect/web/controller/UserController.java
+++ src/main/java/com/yapp18/retrospect/web/controller/UserController.java
@@ -1,4 +1,39 @@
 package com.yapp18.retrospect.web.controller;
 
+import com.yapp18.retrospect.config.ResponseMessage;
+import com.yapp18.retrospect.service.TokenService;
+import com.yapp18.retrospect.service.UserService;
+import com.yapp18.retrospect.web.dto.ApiDefaultResponse;
+import com.yapp18.retrospect.web.dto.UserDto;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@Api(value = "UserController") // swagger 리소스 명시
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+    private final TokenService tokenService;
+
+    @GetMapping("/profiles")
+    public ResponseEntity<Object> getProfiles(HttpServletRequest request){
+        String token = tokenService.getTokenFromRequest(request);
+        Long userIdx = tokenService.getUserIdx(token);
+        UserDto.ProfileResponse response = userService.getUserProfiles(userIdx);
+        return new ResponseEntity<>(ApiDefaultResponse.res(200, ResponseMessage.PROFILE_FIND.getResponseMessage(), response), HttpStatus.OK);
+    }
+
+    @PatchMapping("/profiles")
+    public ResponseEntity<Object> updateProfiles(HttpServletRequest request, @RequestBody UserDto.UpdateRequest updateRequest){
+        String token = tokenService.getTokenFromRequest(request);
+        Long userIdx = tokenService.getUserIdx(token);
+        UserDto.ProfileResponse updated = userService.updateUserProfiles(userIdx, updateRequest);
+        return new ResponseEntity<>(ApiDefaultResponse.res(200, ResponseMessage.PROFILE_UPDATE.getResponseMessage(), updated), HttpStatus.OK);
+    }
 }
