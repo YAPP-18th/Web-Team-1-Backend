641e98950e2200e759a6cb24f36fb0c6a3e3f09e
HurJungMin <gjwjdlas@naver.com>
2021-05-28 16:09:12 +0900

    Add: 공식 템플릿 내용 조회

---
+++ src/main/java/com/yapp18/retrospect/service/TemplateService.java
@@ -0,0 +1,22 @@
+package com.yapp18.retrospect.service;
+
+
+import com.yapp18.retrospect.domain.template.Template;
+import com.yapp18.retrospect.domain.template.TemplateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class TemplateService {
+
+    private final TemplateRepository templateRepository;
+
+    public Optional<String> getTemplateContents(Long templateIdx){
+        return templateRepository.findById(templateIdx).map(Template::getTemplate);
+    }
+}
