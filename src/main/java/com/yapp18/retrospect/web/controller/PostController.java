852f2f2c7731c30c6b847bb798e1151077662ecc
HurJungMin <gjwjdlas@naver.com>
2021-05-28 18:48:19 +0900

    Add: react 배포 경로 CORS 설정에 추가

--- src/main/java/com/yapp18/retrospect/web/controller/PostController.java
+++ src/main/java/com/yapp18/retrospect/web/controller/PostController.java
@@ -93,11 +93,10 @@ public class PostController {
     }
 
     @ApiOperation(value = "mypage", notes = "[마이페이지]회고글 삭제하기")
-    @DeleteMapping("")
+    @DeleteMapping("/{postIdx}")
     public ResponseEntity<Object> deletePosts(HttpServletRequest request,
                                               @ApiParam(value = "회고글 post_idx", required = true, example = "1")
-                                              @RequestParam(value = "postIdx") Long postIdx){
-
+                                              @PathVariable(value = "postIdx") Long postIdx){
         boolean isPost = postService.deletePosts(tokenService.getUserIdx(tokenService.getTokenFromRequest(request)),postIdx);
         if (!isPost){
             return new ResponseEntity<>(ApiDefaultResponse.res(400,"삭제할 idx 없음..."),HttpStatus.BAD_REQUEST);
