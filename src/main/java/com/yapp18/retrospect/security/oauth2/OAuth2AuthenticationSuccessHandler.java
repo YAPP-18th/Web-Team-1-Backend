290fba87e79bcd1277c80c5a0ed80a020574f082
황종훈 <ybell1028@gmail.com>
2021-05-30 10:08:29 +0900

    Fix : 젠킨스 빌드 실패 픽스

--- src/main/java/com/yapp18/retrospect/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/yapp18/retrospect/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
-import sun.tools.jstat.Token;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
