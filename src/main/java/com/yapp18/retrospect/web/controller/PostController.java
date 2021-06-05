1c627ce7b8ca7e36801113cdbcc9a1dc57721ff9
HurJungMin <gjwjdlas@naver.com>
2021-06-05 23:07:05 +0900

    Add : scrap 여부 flag 추가

--- src/main/java/com/yapp18/retrospect/web/controller/PostController.java
+++ src/main/java/com/yapp18/retrospect/web/controller/PostController.java
@@ -30,21 +30,25 @@ public class PostController {
     @ApiOperation(value = "main", notes = "[메인] 회고글 목록 조회, 최신순") // api tag, 설명
     @GetMapping("/lists/new")
     public ResponseEntity<Object> getMainPosts(@ApiParam(value = "현재 페이지 마지막 post_idx", required = true, example = "20")
-                                      @RequestParam(value = "page", defaultValue = "0") Long page,
+                                               HttpServletRequest request,
+                                               @RequestParam(value = "page", defaultValue = "0") Long page,
                                 @RequestParam(value = "pageSize",defaultValue = "20") Integer pageSize){
         if (pageSize == null) pageSize = DEFAULT_SIZE;
+        Long userIdx = (tokenService.getTokenFromRequest(request) != null) ? tokenService.getUserIdx(tokenService.getTokenFromRequest(request)) : 0L;
         return new ResponseEntity<>(ApiDefaultResponse.res(200,ResponseMessage.POST_FIND_RECENT.getResponseMessage(),
-                postService.getPostsListRecent(page, PageRequest.of(0, pageSize))), HttpStatus.OK);
+                postService.getPostsListRecent(page, PageRequest.of(0, pageSize), userIdx)), HttpStatus.OK);
     }
 
     @ApiOperation(value = "main", notes = "[메인] 회고글 목록 조회, 누적조회순")
     @GetMapping("/lists")
     public ResponseEntity<Object> getMainPostsOrderByView(@ApiParam(value = "현재 페이지 마지막 post_idx", required = true, example = "20")
+                                                                      HttpServletRequest request,
                                                @RequestParam(value = "page", defaultValue = "0") Long page,
                                                @RequestParam(value = "pageSize") Integer pageSize){
         if (pageSize == null) pageSize = DEFAULT_SIZE;
+        Long userIdx = (tokenService.getTokenFromRequest(request) != null) ? tokenService.getUserIdx(tokenService.getTokenFromRequest(request)) : 0L;
         return new ResponseEntity<>(ApiDefaultResponse.res(200,ResponseMessage.POST_FIND.getResponseMessage(),
-                postService.getPostsListView(page, PageRequest.of(0,pageSize))), HttpStatus.OK);
+                postService.getPostsListView(page, PageRequest.of(0,pageSize), userIdx)), HttpStatus.OK);
     }
 
 
