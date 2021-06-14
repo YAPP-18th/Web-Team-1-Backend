8e0ce0c84d0f2dcc9f3bb449f743f573065baf94
황종훈 <ybell1028@gmail.com>
2021-06-14 18:04:35 +0900

    Add : 댓글 조회 (무한 스크롤) API 추가

--- src/main/java/com/yapp18/retrospect/domain/like/LikeRepository.java
+++ src/main/java/com/yapp18/retrospect/domain/like/LikeRepository.java
@@ -2,9 +2,11 @@ package com.yapp18.retrospect.domain.like;
 
 import com.yapp18.retrospect.domain.post.Post;
 import com.yapp18.retrospect.domain.user.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
@@ -13,5 +15,9 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     @Query(value = "SELECT like_idx FROM like_tb WHERE (like_tb.post_idx =: postIdx) AND (like_tb.user_idx =: userIdx)",  nativeQuery = true)
     Like findByPostIdxAndUserIdx(Long postIdx, Long userIdx);
 
+    List<Like> findByUserOrderByCreatedAtDesc(User user, Pageable page);
+
+    boolean existsByUserAndLikeIdxLessThan(User user, Long lastIdx);
+//    List<Like> findByUserOrderByCreatedAtDesc(Long userIdx, Pageable page);
 //    Like findByPostAndUser(Post post, User user);
 }
--- src/main/java/com/yapp18/retrospect/service/LikeService.java
+++ src/main/java/com/yapp18/retrospect/service/LikeService.java
@@ -7,12 +7,18 @@ import com.yapp18.retrospect.domain.post.Post;
 import com.yapp18.retrospect.domain.post.PostRepository;
 import com.yapp18.retrospect.domain.user.User;
 import com.yapp18.retrospect.domain.user.UserRepository;
+import com.yapp18.retrospect.mapper.PostMapper;
 import com.yapp18.retrospect.web.advice.EntityNullException;
+import com.yapp18.retrospect.web.dto.ApiPagingResultResponse;
 import com.yapp18.retrospect.web.dto.LikeDto;
+import com.yapp18.retrospect.web.dto.PostDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,10 +26,29 @@ public class LikeService {
     private final LikeRepository likeRepository;
     private final UserRepository userRepository;
     private final PostRepository postRepository;
+    private final PostMapper postMapper;
 
     @Transactional
     public boolean isExist(LikeDto.InputRequest inputRequest, Long userIdx){
         return likeRepository.findByPostIdxAndUserIdx(inputRequest.getPostIdx(), userIdx) != null;
+    }
+
+    @Transactional(readOnly = true)
+    public ApiPagingResultResponse<PostDto.ListResponse> getLikeListCreatedAt(Pageable page, Long userIdx){
+        User user = userRepository.findByUserIdx(userIdx)
+                .orElseThrow(() -> new EntityNullException(ErrorInfo.USER_NULL));
+
+        List<Like> likeList = likeRepository.findByUserOrderByCreatedAtDesc(user, page);
+
+        Long lastIdx = likeList.isEmpty() ? null : likeList.get(likeList.size() - 1).getLikeIdx(); // 낮은 조회수 체크
+
+        List<PostDto.ListResponse> result = likeList.stream()
+                .map(like -> postRepository.findById(like.getPost().getPostIdx())
+                        .orElseThrow(() -> new EntityNullException(ErrorInfo.POST_NULL)))
+                .map(post -> postMapper.postToListResponse(post, userIdx))
+                .collect(Collectors.toList());
+
+        return new ApiPagingResultResponse<>(isNext(user, lastIdx), result);
     }
 
     @Transactional
@@ -44,4 +69,17 @@ public class LikeService {
 
         likeRepository.delete(like);
     }
+
+    // 다음 페이지 여부 확인
+    public boolean isNext(User user, Long cursorId){
+        if (cursorId == null) return false;
+        return likeRepository.existsByUserAndLikeIdxLessThan(user, cursorId);
+    }
+
+    // 누적순 페이징
+//    private List<Like> getLikesAll(Long cursorId, Pageable page){
+//        return
+//                likeRepository.findAllByOrderByCreatedAtDesc(page) :
+//                postRepository.findView(postRepository.findViewByPostIdx(cursorId).getView(), page, postRepository.findCreatedAtByPostIdx(cursorId).getCreatedAt());
+//    }
 }
