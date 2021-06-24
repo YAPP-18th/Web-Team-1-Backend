92b41984342463528306f62e1e2a2d3d33fa30bf
HurJungMin <gjwjdlas@naver.com>
2021-06-24 22:07:25 +0900

    Add : 최근 읽은 글 페이징

--- src/main/java/com/yapp18/retrospect/service/ListService.java
+++ src/main/java/com/yapp18/retrospect/service/ListService.java
@@ -8,10 +8,12 @@ import com.yapp18.retrospect.domain.post.PostRepository;
 import com.yapp18.retrospect.domain.recent.RecentLog;
 import com.yapp18.retrospect.mapper.PostMapper;
 import com.yapp18.retrospect.web.advice.EntityNullException;
+import com.yapp18.retrospect.web.dto.ApiPagingResultResponse;
 import com.yapp18.retrospect.web.dto.PostDto;
 import com.yapp18.retrospect.web.dto.RedisRequestDto;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -23,10 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.sql.Date;
 import java.sql.Timestamp;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -55,22 +54,30 @@ public class ListService {
 
     // 최근 읽은 글 조회
     @Transactional
-    public List<PostDto.ListResponse> findRecentPosts(Long userIdx) {
+    public ApiPagingResultResponse<PostDto.ListResponse> findRecentPosts(Long userIdx, Long page, Integer pageSize) {
         ZSetOperations<String, RecentLog> zSetOps = redisTemplate.opsForZSet();
-        // if redis에 key가 있으면
-        ObjectMapper objectMapper = new ObjectMapper(); // linkedHashMap으로 저장된 redis 값들을 List로 변환해줌
-        List<RecentLog> result = objectMapper.convertValue(Objects.requireNonNull(zSetOps.reverseRange(setKey(userIdx), 0, -1)),
+        // 페이징 start, end 인덱스
+        HashMap<String, Integer> pagingIdx = getPagingIndex(page, pageSize);
+        System.out.println("시작"+ pagingIdx.get("start") + " 끝 "+ pagingIdx.get("end"));
+        // linkedHashMap 으로 저장된 redis 값들을 List로 변환해줌
+        long size = zSetOps.size(setKey(userIdx));
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<RecentLog> result = objectMapper.convertValue(Objects.requireNonNull(zSetOps.reverseRange(setKey(userIdx),
+                pagingIdx.get("start"),
+                pagingIdx.get("end"))),
                 new TypeReference<List<RecentLog>>() {
         });
-        return result.stream().map(x -> postMapper.postToListResponse(findPostById(x.getPostIdx()), userIdx)).collect(Collectors.toList());
+        return new ApiPagingResultResponse<>(isNextRedis(size, page, pageSize),
+                result.stream().map(x -> postMapper.postToListResponse(findPostById(x.getPostIdx()), userIdx)).collect(Collectors.toList()))
+                ;
 
     }
     // redis 키 여부 체크
-//    private boolean isKey(String key){
-//
-//    }
+    private boolean isKey(String key){
+        return redisTemplate.hasKey(key);
+    }
 
-    // redis key 생성
+    // redis key String 생성
     private String setKey(Long userIdx){
         return "userIdx::"+userIdx;
     }
@@ -81,7 +88,6 @@ public class ListService {
                 .orElseThrow(() -> new EntityNullException(ErrorInfo.POST_NULL));
     }
 
-
     // redis에서 value 삭제
     public void deleteRedisPost(Long userIdx,Long postIdx){
         ZSetOperations<String, RecentLog> zSetOps = redisTemplate.opsForZSet();
@@ -89,6 +95,7 @@ public class ListService {
         zSetOps.remove(setKey(userIdx), recentLog);
     }
 
+    // redis에 해당 postIdx 있는지 확인
     public boolean isPostsExist(Long userIdx, Long postIdx){
         ZSetOperations<String, RecentLog> zSetOps = redisTemplate.opsForZSet();
         RecentLog recentLog = RecentLog.builder().userIdx(userIdx).postIdx(postIdx).build();
@@ -99,5 +106,21 @@ public class ListService {
         return result.contains(recentLog);
     }
 
+    // redis 결과 페이징
+    private HashMap<String,Integer> getPagingIndex(Long page, Integer pageSize){
+        System.out.println("pageable 정보:" + page + "//" + pageSize);
+        int start = (int) (page * pageSize); // page, pageSize
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("start", start);
+        map.put("end", start + pageSize -1);
+        return map;
+    }
 
+    // 페이징 시 다음 결과 있는지 검사
+    private boolean isNextRedis(long size, Long page ,Integer pageSize){
+        if (page+1 >  Math.round(size/pageSize)){
+            return false;
+        }
+        return true;
+    }
 }
